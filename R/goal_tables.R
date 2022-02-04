e5fd494e8f266007214a05a10f405e2ddd321eb3
dgerth5 <davidgerth5@gmail.com>
2022-02-03 20:31:38 -0500

    new gt

--- R/goal_tables.R
+++ R/goal_tables.R
@@ -1,3 +1,16 @@
+#' @title FUNCTION_TITLE
+#' @description FUNCTION_DESCRIPTION
+#' @param year PARAM_DESCRIPTION
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples 
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @rdname goal_table
+#' @export 
 goal_table <- function(year){
   
   require(rvest)
